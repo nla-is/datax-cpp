5add5664866fdbb6f53b57c5bc8cf58d423c5a39
Giuseppe Coviello <gco@nec-labs.com>
2021-10-22 11:21:19 -0400

    Project created

---
+++ datax.cpp
@@ -0,0 +1,7 @@
+#include "datax.h"
+
+#include <iostream>
+
+void hello() {
+  std::cout << "Hello, World!" << std::endl;
+}
---
+++ datax.h
@@ -0,0 +1,6 @@
+#ifndef _DATAX_H
+#define _DATAX_H
+
+void hello();
+
+#endif //_DATAX_H
