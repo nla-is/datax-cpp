ab628e7bbad89f18344b11b5ca7c53b361c22739
Giuseppe Coviello <gco@nec-labs.com>
2021-10-22 11:49:13 -0400

    Implemented DataX::Configuration()

--- datax.cpp
+++ datax.cpp
@@ -1,10 +1,11 @@
 #include "datax.h"
 
+#include <cstdlib>
+#include <fstream>
+
 class Implementation : public datax::DataX {
  public:
-  nlohmann::json Configuration() override {
-    return nlohmann::json();
-  }
+  nlohmann::json Configuration() override;
   datax::RawMessage NextRaw() override {
     return datax::RawMessage();
   }
@@ -26,6 +27,23 @@ class Implementation : public datax::DataX {
     return instance;
   }
 };
+
+std::string Getenv(const std::string &variable) {
+  auto value = getenv(variable.c_str());
+  if (value == nullptr) {
+    return "";
+  }
+  return {value};
+}
+
+nlohmann::json Implementation::Configuration() {
+  auto configurationPath = Getenv("DATAX_CONFIGURATION");
+  if (configurationPath.empty()) {
+    configurationPath = "/datax/configuration";
+  }
+  std::ifstream in(configurationPath);
+  return nlohmann::json::parse(in);
+}
 
 std::shared_ptr<datax::DataX> datax::New() {
   return Implementation::Instance();
--- test-sdk.cpp
+++ test-sdk.cpp
@@ -4,6 +4,10 @@
 
 #include "datax.h"
 
+#include <iostream>
+
 int main() {
   auto dx = datax::New();
+
+  std::cout << "Configuration: " << dx->Configuration() << std::endl;
 }
