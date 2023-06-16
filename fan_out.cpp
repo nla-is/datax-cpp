8486be4f603cfa940b45b1b27b34537cbe308ae2
Giuseppe Coviello <gco@nec-labs.com>
2023-06-16 12:13:02 -0400

    Added debug output to fan out

--- fan_out.cpp
+++ fan_out.cpp
@@ -8,9 +8,11 @@
 #include "datax.h"
 #include "implementation.h"
 
+using datax::sdk::implementation::Queue;
+
 namespace datax {
 WithFanOut::WithFanOut(std::function<std::shared_ptr<FanOutHandler>()> fanOutHandlerFactory) :
-fanOutHandlerFactory(std::move(fanOutHandlerFactory)) {}
+    fanOutHandlerFactory(std::move(fanOutHandlerFactory)) {}
 
 void WithFanOut::Apply(DataX *_implementation) const {
   auto implementation = reinterpret_cast<datax::sdk::implementation::Implementation *>(_implementation);
@@ -22,13 +24,17 @@ void WithFanOut::Apply(DataX *_implementation) const {
   }
 
   for (auto i = 0; i < number_of_handlers; i++) {
-    std::thread t([&](){
-      auto handler = fanOutHandlerFactory();
+    fprintf(stderr, "[DataX] Creating fan-out handler %d\n", i);
+    auto handler = fanOutHandlerFactory();
+    fprintf(stderr, "[DataX] Created fan-out handler %d\n", i);
+    std::thread t([](int i, const std::shared_ptr<FanOutHandler> &handler,
+                     const std::shared_ptr<Queue<nlohmann::json>> &inputQueue,
+                     const std::shared_ptr<Queue<nlohmann::json>> &outputQueue) {
       while (true) {
-        auto msg = implementation->fanOutInputQueue->pop();
-        implementation->fanOutOutputQueue->push(handler->Process(msg));
+        auto msg = inputQueue->pop();
+        outputQueue->push(handler->Process(msg));
       }
-    });
+    }, i, handler, implementation->fanOutInputQueue, implementation->fanOutOutputQueue);
     t.detach();
   }
 }
