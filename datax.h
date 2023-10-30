7a6334ffb96dec2ffc34aa4bb6df15f8fe8e76fe
Giuseppe Coviello <gco@nec-labs.com>
2023-10-30 17:02:15 -0400

    Removed unused files

--- datax.h
+++ datax.h
@@ -1,16 +1,9 @@
-#ifndef _DATAX_H
-#define _DATAX_H
+#pragma once
 
 #include <json.hpp>
 
 namespace datax {
 class Exception : public std::exception {
-};
-
-struct RawMessage {
-  std::string Reference;
-  std::string Stream;
-  std::vector<unsigned char> Data;
 };
 
 struct Message {
@@ -21,36 +14,10 @@ struct Message {
 
 class DataX {
  public:
-  virtual nlohmann::json Configuration() = 0;
-  virtual RawMessage NextRaw() = 0;
-  virtual Message Next() = 0;
-  virtual void Emit(const nlohmann::json &message, const std::string &reference = "") = 0;
-  virtual void EmitRaw(const std::vector<unsigned char> &data, const std::string &reference = "") = 0;
-  virtual void EmitRaw(const unsigned char *data, int dataSize, const std::string &reference = "") = 0;
-  virtual std::vector<nlohmann::json> FanOut(const std::vector<nlohmann::json> &messages) = 0;
-};
-
-class FanOutHandler {
- public:
-  virtual nlohmann::json Process(const nlohmann::json &message) = 0;
-};
-
-class Option {
- public:
-  virtual void Apply(DataX *implementation) const = 0;
-};
-
-class WithFanOut : public Option {
- public:
-  explicit WithFanOut(std::function<std::shared_ptr<FanOutHandler>()> fanOutHandlerFactory);
-  void Apply(DataX *implementation) const override;
-
- private:
-  std::function<std::shared_ptr<FanOutHandler>()> fanOutHandlerFactory;
+  nlohmann::json Configuration() = 0;
+  Message Next() = 0;
+  void Emit(const nlohmann::json &message, const std::string &reference = "") = 0;
 };
 
 std::shared_ptr<DataX> New();
-std::shared_ptr<DataX> New(std::initializer_list<Option *> options);
 }
-
-#endif //_DATAX_H
